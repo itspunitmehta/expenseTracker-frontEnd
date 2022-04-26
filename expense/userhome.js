fa05343a17d2bade3be7858b04b9e25d790384a6
itspunitmehta <rockz1918@gmail.com>
2022-04-26 13:54:07 +0530

    added expenses to the dom

--- expense/userhome.js
+++ expense/userhome.js
@@ -1,3 +1,4 @@
+const loginToken = localStorage.getItem('token');
 function addExpense(event){
     event.preventDefault();
     const form = new FormData(event.target);
@@ -9,18 +10,46 @@ function addExpense(event){
 
     }
     console.log(expenseDetails);
-    const loginToken = localStorage.getItem('token');
+    axios.post('http://localhost:8000/user/addexpenses',expenseDetails,{headers: {authorization:loginToken} })
+    .then(response=>{
+        if(response===201){
+            addNewExpensetoDOM(res.data.expense);
+        }else{
+            throw new Error('Failed')
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+    })
 }
 
-function addNewExpensetoUI(expense){
+function addNewExpensetoDOM(expense){
     const parentElement = document.getElementById('listOfExpenses');
-    // const expenseElemId = `expense-${expense.id}`;
-    const expenseElemId = `expense-${expense.category}`;
+    const expenseElemId = `expense-${expense.id}`;
     parentElement.innerHTML += `
         <li id=${expenseElemId}>
-            ${expense.expenseamount} - ${expense.category} - ${expense.description}
+            ${expense.amount} - ${expense.category} - ${expense.description}
             <button onclick='deleteExpense(event, ${expense.id})'>
                 Delete Expense
             </button>
         </li>`
 }
+
+window.addEventListener('load',()=>{
+    axios.get('http://localhost:8000/user/getexpenses',{headers: {authorization:loginToken} })
+    .then(response=>{
+        if(response.status===200){
+            response.data.expenses.forEach(expense => {
+                addNewExpensetoDOM(expense)
+            })
+        }else{
+            console.log(response.status);
+            throw new Error('Failed To Load')
+        }
+    })
+    .catch(err=>{
+        console.log(err);
+        alert('Please Log-In First');
+        window.location.href = "../login/login.html"
+    })
+})
